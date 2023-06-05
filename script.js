d5797a960837f443eeedaf491a8c66bb6c2217f7
HossainRafi <hossainrafi71@gmail.com>
2023-06-05 10:47:33 +0600

    get ingredients

--- script.js
+++ script.js
@@ -14,4 +14,18 @@ fetch(url)
     console.log(myMeal.strMeal);
     console.log(myMeal.strArea);
     console.log(myMeal.strInstructions);
+
+    let count = 1;
+    let ingredients = [];
+    for (let i in myMeal) {
+      let ingredient = "";
+      let measure = "";
+      if (i.startsWith("strIngredient") && myMeal[i]) {
+        ingredient = myMeal[i];
+        measure = myMeal[`strMeasure` + count];
+        count += 1;
+        ingredients.push(`${measure} ${ingredient}`);
+      }
+    }
+    console.log(ingredients);
   });
