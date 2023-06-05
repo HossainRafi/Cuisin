c00d5a0e54ff201471b1cbcb53624c55a69b3bb6
HossainRafi <hossainrafi71@gmail.com>
2023-06-05 10:15:09 +0600

    fetch data

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
 let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-
 let userInp = document.getElementById("user-inp").value;
+
+fetch(url + userInp)
+  .then((response) => response.json())
+  .then((data) => {
+    let myMeal = data.meals[0];
+    console.log(myMeal);
+    console.log(myMeal.strMealThumb);
+    console.log(myMeal.strMeal);
+    console.log(myMeal.strArea);
+    console.log(myMeal.strInstructions);
+  });
